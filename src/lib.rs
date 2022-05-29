4c2e51a44df21dd867ab555a6809f46a527746b7
Yeicor <4929005+Yeicor@users.noreply.github.com>
2022-05-29 14:22:49 +0200

    Lots of improvements, mainly to input handling, logging and modularity

--- src/lib.rs
+++ src/lib.rs
@@ -2,31 +2,25 @@
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
-use run::MyEguiApp;
+mod app;
+mod input;
 
-mod run;
-
+// === Entry point for web ===
 #[cfg(target_arch = "wasm32")]
 #[wasm_bindgen(start)]
 pub async fn start() -> Result<(), JsValue> {
-    console_log::init_with_level(log::Level::Debug).unwrap();
-
-    use log::info;
-    info!("Logging works!");
-
+    // console_log::init_with_level(log::Level::Debug).unwrap();
+    tracing_wasm::set_as_global_default();
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
 
-    eframe::start_web(env!("CARGO_PKG_NAME"), Box::new(|cc| Box::new(MyEguiApp::new(cc))))?;
+    eframe::start_web(env!("CARGO_PKG_NAME"), Box::new(|cc| Box::new(app::SDFViewerApp::new(cc))))?;
     Ok(())
 }
 
+// === Entry point for android ===
 #[cfg(target_os = "android")]
 #[cfg_attr(target_os = "android", ndk_glue::main(backtrace = "full"))] // TODO: , logger(level = "debug", tag = "rust.sdf-viewer")
 // #[tokio::main] // Not compatible with eframe :(
 pub fn main() {
-    use log::info;
-    info!("Logging works!");
-
-    let native_options = eframe::NativeOptions::default();
-    eframe::run_native("SDF Viewer", native_options, Box::new(|cc| Box::new(MyEguiApp::new(cc))));
+    crate::main();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
-use run::MyEguiApp;
+mod app;
+mod input;
 
-mod run;
-
-// Entry point for non-wasm
+// === Entry point for desktop ===
 #[cfg(not(any(target_arch = "wasm32")))]
 // #[tokio::main] // Not compatible with eframe :(
 #[allow(dead_code)] // Fix for clippy
-fn main() {
-    use log::info;
-    info!("Logging works!");
-
-    #[cfg(not(target_arch = "wasm32"))] // <- eframe::run_native does not exist for web
-    {
-        let native_options = eframe::NativeOptions::default();
-        eframe::run_native("SDF Viewer", native_options, Box::new(|cc| Box::new(MyEguiApp::new(cc))));
-    }
+pub(crate) fn main() {
+    // Setup logging
+    tracing::subscriber::set_global_default(tracing_subscriber::FmtSubscriber::default())
+        .expect("Failed to set global default subscriber");
+    // Run app
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native("SDF Viewer", native_options, Box::new(|cc|
+        Box::new(app::SDFViewerApp::new(cc))));
 }
